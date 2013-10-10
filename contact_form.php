5f7c950f8125a0c828c0e77b4cb38382e5094577
webdorado <webdorado@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-10 13:23:39 +0000

    Add description
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-maker/tags/1.5.8@785893 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- contact_form.php
+++ contact_form.php
@@ -2,6 +2,7 @@
 /*
 Plugin Name: Contact Form FREE
 Plugin URI: http://web-dorado.com/products/form-maker-wordpress.html
+Description: WordPress Contact Form Maker is an advanced and easy-to-use tool for creating forms. The backend interface is intuitive and user friendly which allows users far from scripting and programming to create WordPress Forms.
 Version: 1.5.8
 Author: http://web-dorado.com/
 License: GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
